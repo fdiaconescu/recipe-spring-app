5ac5355e99f0871022e63ee7dcda72f1f752f6a5
fdiaconescu <fdiaconescu@hotmail.com>
2018-12-21 17:42:28 -0800

    Initial commit

---
+++ src/main/java/fd/spring5/recipes/domain/Recipe.java
@@ -0,0 +1,13 @@
+package fd.spring5.recipes.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Byte[] image;
+}
