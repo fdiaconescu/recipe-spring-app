26e2094b21745b48fc87b60661070559295248a4
fdiaconescu <fdiaconescu@hotmail.com>
2018-12-23 05:54:39 -0800

    add jpa query methods

--- src/main/java/fd/spring5/recipes/repositories/CategoryRepository.java
+++ src/main/java/fd/spring5/recipes/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package fd.spring5.recipes.repositories;
 import fd.spring5.recipes.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/fd/spring5/recipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/fd/spring5/recipes/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package fd.spring5.recipes.repositories;
 import fd.spring5.recipes.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
